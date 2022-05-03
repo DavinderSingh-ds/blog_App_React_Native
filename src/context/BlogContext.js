489235e9c50d178befee3748accbaa70cf85017e
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-03 08:41:54 +0530

    Rendering a List of blogPosts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,9 +4,16 @@ const BlogContext = React.createContext();
 
 // Blog Provider is not default 
 export const BlogProvider = ({ children }) => {
+    const blogPosts = [
+        {title : 'Blog Post #1'},
+        {title : 'Blog Post #2'},
+    ];
 
     return (
-        <BlogContext.Provider value = {["Hi I am Davinder"," "," Hi guys"," ",1," ",2," ",4]}>
+        // <BlogContext.Provider value = {["Hi I am Davinder"," "," Hi guys"," ",1," ",2," ",4]}>
+        //     {children}
+        // </BlogContext.Provider>
+        <BlogContext.Provider value = {blogPosts}>
             {children}
         </BlogContext.Provider>
     );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,27 @@
 import React,{useContext} from 'react';
-import {Text,View,StyleSheet} from 'react-native';
+import {Text,View,StyleSheet,FlatList} from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
+    // const value = useContext(BlogContext);
+    const blogPosts = useContext(BlogContext);
 
     return (
         <View>
             <Text>Index Screen</Text>
-            <Text>{value}</Text>
+            {/* <Text>{value}</Text> */}
+            <FlatList 
+                data = {blogPosts}
+                //it communicate with array of its key like title is key of blogPosts in BlogContext.js
+                keyExtractor = {(blogPost) => blogPost.title}
+                renderItem = {({item}) => {
+                    return (
+                        <Text>
+                            {item.title}
+                        </Text>
+                    );
+                }}
+            />
         </View>
     );
 };
