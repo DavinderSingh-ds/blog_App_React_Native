5e2dad5406af1c2c4fb472dc4ec1da896869ee22
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-03 07:46:50 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import IndexScreen from './src/screens/IndexScreen';
+
+const Stack = createNativeStackNavigator();
+
+const navigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="IndexSc">
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Stack.Screen 
+            name="IndexSc"
+            component={IndexScreen} 
+            options={{ title: 'Index Screen' }}
+            />  
+
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  );
+};
+
+export default navigator;
