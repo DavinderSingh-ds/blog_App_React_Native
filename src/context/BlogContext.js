2598f3b90ae5251bce10f920b9096a3fa198136a
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-03 08:31:57 +0530

    Moving Data with Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,15 @@
-import React, { createContext } from 'react';
+import React from 'react';
 
 const BlogContext = React.createContext();
 
+// Blog Provider is not default 
 export const BlogProvider = ({ children }) => {
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value = {["Hi I am Davinder"," "," Hi guys"," ",1," ",2," ",4]}>
             {children}
         </BlogContext.Provider>
     );
 };
+
+export default BlogContext;
