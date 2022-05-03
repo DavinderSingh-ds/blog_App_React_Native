f8e5142996418c12c5a1d6f91d285a1e35e54169
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-03 13:21:40 +0530

    textInput

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,19 @@
-import React, {useContext} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View, Text, TextInput ,StyleSheet} from 'react-native';
 
 const CreateScreen = ({ navigation,route}) => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
     return (
         <View>
-            <Text>Create</Text>
+
+            <Text>Enter Title: </Text>
+            <TextInput value = {title} onChangeText = {(text) => setTitle(text)}/>
+
+            <Text>Enter Content: </Text>
+            <TextInput value = {content} onChangeText = {(text) => setContent(text)} />
+            
         </View>
     );
 };
